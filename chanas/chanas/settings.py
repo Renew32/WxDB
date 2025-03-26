9f4c7be7d883fe2567b2c742fedc76e13d3caf3c
Antoine Missoup Konga <mcblum19@yahoo.com>
2025-03-26 15:01:21 +0100

    pip install psycopg2-binary

--- chanas/chanas/settings.py
+++ chanas/chanas/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'chanas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prestaDB',
+        'USER': 'utilisateur',
+        'PASSWORD': 'mot_de_passe',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
