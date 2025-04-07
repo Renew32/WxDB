ff20338b24b4d8c5825cbd1c7184f6cdc1ca589f
Antoine Missoup Konga <mcblum19@yahoo.com>
2025-04-07 21:42:26 +0100

    Update settings.py

--- chanas/settings.py
+++ chanas/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-#3&y$5y&pyywoes!o+1_@1l9=b@_0l1b^w(iqy*j!3-!=v%&&a
 DEBUG = False
 
 
-# ALLOWED_HOSTS = ['wxdbx.onrender.com']
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS = ['wxdbx.onrender.com']
+
 
 
 # Application definition
